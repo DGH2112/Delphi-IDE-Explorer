b083177b8d5a24385497ac6b08f16fbcb3d82932
DGH2112 <davidghoyle@gmail.com>
2016-12-17 18:00:38 +0000

    Initial import into GitHub
    Initial import of this project into GitHub.

---
+++ Source/DGHExplFrm.pas
@@ -0,0 +1,431 @@
+(**
+
+  This module contains the explorer form interface.
+
+  @Date    17 Apr 2016
+  @Version 2.0
+  @Author  David Hoyle
+
+**)
+Unit DGHExplFrm;
+
+Interface
+
+Uses
+  Windows,
+  Messages,
+  SysUtils,
+  Classes,
+  Graphics,
+  Controls,
+  Forms,
+  Dialogs,
+  ComCtrls,
+  ExtCtrls,
+  ImgList;
+
+Type
+  (** This class represent a form for displaying the internal published elements of the
+      IDE. **)
+  TDGHIDEExplorerForm = Class(TForm)
+    tvComponentTree: TTreeView;
+    ilImageList1: TImageList;
+    splSplitter1: TSplitter;
+    ilTypeKindImages: TImageList;
+    pgcPropertiesMethodsAndEvents: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    lvOldProperties: TListView;
+    tvHierarchies: TTreeView;
+    tabNewProperties: TTabSheet;
+    lvProperties: TListView;
+    tabFields: TTabSheet;
+    lvFields: TListView;
+    tabEvents: TTabSheet;
+    lvEvents: TListView;
+    tabMethods: TTabSheet;
+    lvMethods: TListView;
+    ilScope: TImageList;
+    Procedure BuildFormComponentTree(Sender: TObject);
+    Procedure tvComponentTreeChange(Sender: TObject; Node: TTreeNode);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  Strict Private
+    {Private declarations}
+    Procedure GetComponents(Node: TTreeNode; Component: TComponent);
+    Procedure LoadSettings;
+    Procedure SaveSettings;
+    Procedure BuildComponentHeritage(Node : TTreeNode);
+    Procedure BuildParentHeritage(Node : TTreeNode);
+  Strict Protected
+  Public
+    {Public declarations}
+  End;
+
+Implementation
+
+{$R *.DFM}
+
+
+Uses
+  Registry,
+  DGHRTTIFunctions,
+  DGHOLDRTTIFunctions;
+
+Const
+  (** This is the root registration jey for this applications settings. **)
+  RegKey = '\Software\Seasons Fall';
+  (** This is the section name for the applications settings in the registry **)
+  SectionName = 'DGH IDE Explorer';
+
+(**
+
+  This is an OnFormCreate Event Handler for the TDGHIDEExplorerForm class.
+
+  @precon  None.
+  @postcon Loads the applications settings.
+
+  @param   Sender as a TObject
+
+**)
+Procedure TDGHIDEExplorerForm.FormCreate(Sender: TObject);
+
+Begin
+  LoadSettings;
+End;
+
+(**
+
+  This is an OnFormDestroy Event Handler for the TDGHIDEExplorerForm class.
+
+  @precon  None.
+  @postcon Saves the applications settings.
+
+  @param   Sender as a TObject
+
+**)
+Procedure TDGHIDEExplorerForm.FormDestroy(Sender: TObject);
+
+Begin
+  SaveSettings;
+End;
+
+(**
+
+  This is an on form show event handler for the form.
+
+  @precon  None.
+  @postcon Builds the forms component tree.
+
+  @note    Its MUCH MORE responsive here in the OnShow event than in the OnCreate event.
+
+  @param   Sender as a TObject
+
+**)
+Procedure TDGHIDEExplorerForm.FormShow(Sender: TObject);
+
+Begin
+  BuildFormComponentTree(Sender);
+  pgcPropertiesMethodsAndEvents.ActivePageIndex := 0;
+End;
+
+(**
+
+  This is the forms on show event. It initialises the tree view.
+
+  @precon  None.
+  @postcon Iterates through the forms and datamodules and adds them to the tree view.
+
+  @param   Sender as a TObject
+
+**)
+Procedure TDGHIDEExplorerForm.BuildFormComponentTree(Sender: TObject);
+
+  (**
+
+    This method adds a form/datamodule to a root node in the tree and then calls
+    GetComponents to get the forms components.
+
+    @precon  ParentNode and ScreenForm must be valid instances.
+    @postcon Adds a form/datamodule to a root node in the tree and then calls
+             GetComponents to get the forms components.
+
+    @param   ParentNode as a TTreeNode
+    @param   ScreenForm as a TComponent
+
+  **)
+  Procedure IterateForms(ParentNode : TTreeNode; ScreenForm : TComponent);
+
+  Var
+    Item: TTreeNode;
+
+  Begin
+    Item := tvComponentTree.Items.AddChildObject(ParentNode, ScreenForm.Name + ' : ' +
+      ScreenForm.ClassName, ScreenForm);
+    Item.ImageIndex := 2;
+    Item.SelectedIndex := 2;
+    //: @bug Causes IDE to crash ProcessClass(tvComponentTree, Item, ScreenForm);
+    GetComponents(Item, ScreenForm);
+  End;
+
+Var
+  i: Integer;
+  nApplication : TTreeNode;
+  nScreen : TTreeNode;
+  N: TTreeNode;
+
+Begin
+  With tvComponentTree Do
+    Begin
+      Items.BeginUpdate;
+      Try
+        FoundClasses.Clear;
+        nApplication := tvComponentTree.Items.AddChildObject(Nil, 'Application', Application);
+        //: @bug Causes IDE to crash ProcessClass(tvComponentTree, nApplication, Application);
+        nScreen := tvComponentTree.Items.AddChildObject(Nil, 'Screen', Screen);
+        //: @bug Causes IDE to crash ProcessClass(tvComponentTree, nScreen, Screen);
+        N := tvComponentTree.Items.AddChild(nScreen, 'Forms');
+        For i := 0 To Screen.FormCount - 1 Do
+          IterateForms(N, Screen.Forms[i]);
+        N := tvComponentTree.Items.AddChild(nScreen, 'CustomForms');
+        For i := 0 To Screen.CustomFormCount - 1 Do
+          IterateForms(N, Screen.CustomForms[i]);
+        N := tvComponentTree.Items.AddChild(nScreen, 'DataModules');
+        For i := 0 To Screen.DataModuleCount - 1 Do
+         IterateForms(N, Screen.DataModules[i]);
+        nApplication.Expand(False);
+        nScreen.Expand(False);
+      Finally
+        Items.EndUpdate;
+      End;
+    End;
+End;
+
+(**
+
+  This method iterates the IDE structure and populates the tree view.
+
+  @precon  Node and Component must be valid instances.
+  @postcon Each component of the parent component is added to the given node.
+
+  @param   Node      as a TTreeNode
+  @param   Component as a TComponent
+
+**)
+Procedure TDGHIDEExplorerForm.GetComponents(Node: TTreeNode; Component: TComponent);
+
+Var
+  i: Integer;
+  NewNode: TTreeNode;
+
+Begin
+  For i := 0 To Component.ComponentCount - 1 Do
+    Begin
+      NewNode := tvComponentTree.Items.AddChild(Node, Component.Components[i].Name + ' : ' +
+        Component.Components[i].ClassName);
+      NewNode.Data := Pointer(Component.Components[i]);
+      NewNode.ImageIndex := 3;
+      NewNode.SelectedIndex := 3;
+      GetComponents(NewNode, Component.Components[i]);
+    End;
+End;
+
+(**
+
+  This method loads the applications settings from the registry.
+
+  @precon  None.
+  @postcon The applications settings are loaded from the regsitry.
+
+**)
+Procedure TDGHIDEExplorerForm.LoadSettings;
+
+Var
+  i : Integer;
+  lv: TListView;
+  j: Integer;
+
+Begin
+  With TRegIniFile.Create(RegKey) Do
+    Try
+      Self.Top := ReadInteger(SectionName, 'Top', 200);
+      Self.Left := ReadInteger(SectionName, 'Left', 200);
+      Self.Width := ReadInteger(SectionName, 'Width', 450);
+      Self.Height := ReadInteger(SectionName, 'Height', 300);
+      tvComponentTree.Width := ReadInteger(SectionName, 'TreeWidth', 200);
+      For i := 0 To ComponentCount - 1 Do
+        If Components[i] Is TListView Then
+          Begin
+            lv := Components[i] As TListView;
+            For j := 0 To lv.Columns.Count - 1 Do
+              lv.Columns[j].Width := ReadInteger(lv.Name, lv.Column[j].Caption, 100);
+          End;
+    Finally
+      Free;
+    End;
+End;
+
+(**
+
+  This method builds a hierarchical list of the components heritage.
+
+  @precon  Node must be a vldi instance.
+  @postcon The heritage tree is output to the hierarchies tab.
+
+  @param   Node as a TTreeNode
+
+**)
+Procedure TDGHIDEExplorerForm.BuildComponentHeritage(Node : TTreeNode);
+
+Var
+  PNode: TTreeNode;
+  ClassRef: TClass;
+  strList: TStringList;
+  i: Integer;
+
+Begin
+  tvHierarchies.Items.BeginUpdate;
+  Try
+    If (Node <> Nil) And (Node.Data <> Nil) Then
+      Begin
+        ClassRef := TComponent(Node.Data).ClassType;
+        strList := TStringList.Create;
+        Try
+          While ClassRef <> Nil Do
+            Begin
+              strList.Insert(0, ClassRef.ClassName);
+              ClassRef := ClassRef.ClassParent;
+            End;
+          PNode := tvHierarchies.Items.AddChild(Nil, 'Heritage');
+          For i := 0 To strList.Count - 1 Do
+            Begin
+              PNode := tvHierarchies.Items.AddChild(PNode, strList[i]);
+              PNode.ImageIndex := 3;
+              PNode.SelectedIndex := 3;
+            End;
+        Finally
+          strList.Free;
+        End;
+      End;
+  Finally
+    tvHierarchies.Items.EndUpdate;
+  End;
+End;
+
+(**
+
+  This method outputs the parent hierarchy of the WinControl (if its a WinControl).
+
+  @precon  Node must be a vldi instance.
+  @postcon The parent hierarchy is outuput to the hierarchies tab.
+
+  @param   Node as a TTreeNode
+
+**)
+Procedure TDGHIDEExplorerForm.BuildParentHeritage(Node : TTreeNode);
+
+Var
+  PNode: TTreeNode;
+  Parent: TWinControl;
+  slList: TStringList;
+  i: Integer;
+
+Begin
+  tvHierarchies.Items.BeginUpdate;
+  Try
+    If (Node <> Nil) And (Node.Data <> Nil) And (TObject(Node.Data) Is TWinControl) Then
+      Begin
+        Parent := TWinControl(Node.Data).Parent;
+        slList := TStringList.Create;
+        Try
+          slList.Add(TWinControl(Node.Data).Name + ' : ' + TWinControl(Node.Data).ClassName);
+          While Parent <> Nil Do
+            Begin
+              slList.Insert(0, Parent.Name + ' : ' + Parent.ClassName);
+              Parent := Parent.Parent;
+            End;
+          PNode := tvHierarchies.Items.AddChild(Nil, 'Parentage');
+          For i := 0 To slList.Count - 1 Do
+            Begin
+              PNode := tvHierarchies.Items.AddChild(PNode, slList[i]);
+              PNode.ImageIndex := 3;
+              PNode.SelectedIndex := 3;
+            End;
+        Finally
+          slList.Free;
+        End;
+      End;
+  Finally
+    tvHierarchies.Items.EndUpdate;
+  End;
+End;
+
+(**
+
+  This method saves the applications settings to the registry.
+
+  @precon  None.
+  @postcon The applications settings are saved to the registry.
+
+**)
+Procedure TDGHIDEExplorerForm.SaveSettings;
+
+Var
+  i, j : Integer;
+  lv : TListView;
+
+Begin
+  With TRegIniFile.Create(RegKey) Do
+    Try
+      WriteBool(SectionName, 'Visible', Visible);
+      WriteInteger(SectionName, 'Top', Self.Top);
+      WriteInteger(SectionName, 'Left', Self.Left);
+      WriteInteger(SectionName, 'Width', Self.Width);
+      WriteInteger(SectionName, 'Height', Self.Height);
+      WriteInteger(SectionName, 'TreeWidth', tvComponentTree.Width);
+      For i := 0 To ComponentCount - 1 Do
+        If Components[i] Is TListView Then
+          Begin
+            lv := Components[i] As TListView;
+            For j := 0 To lv.Columns.Count - 1 Do
+              WriteInteger(lv.Name, lv.Column[j].Caption, lv.Columns[j].Width);
+          End;
+    Finally
+      Free;
+    End;
+End;
+
+(**
+
+  This is the tree views on change event handler. It gets the item selecteds properties
+  and displays them.
+
+  @precon  None.
+  @postcon Clears the list views and re-populates them with data for the new selected
+           node.
+
+  @param   Sender as a TObject
+  @param   Node   as a TTreeNode
+
+**)
+Procedure TDGHIDEExplorerForm.tvComponentTreeChange(Sender: TObject; Node: TTreeNode);
+
+Begin
+  lvFields.Clear;
+  lvMethods.Clear;
+  lvProperties.Clear;
+  lvEvents.Clear;
+  lvOldProperties.Clear;
+  If (Node <> Nil) And (Node.Data <> Nil) Then
+    Begin
+      ProcessObject(Node.Data, lvFields, lvMethods, lvProperties, lvEvents);
+      ProcessOldProperties(lvOldProperties, Node.Data);
+    End;
+  tvHierarchies.Items.Clear;
+  BuildComponentHeritage(Node);
+  BuildParentHeritage(Node);
+  tvHierarchies.FullExpand;
+End;
+
+End.
---
+++ Source/DGHRTTIFunctions.pas
@@ -0,0 +1,530 @@
+(**
+
+  This module contains the new RTTI code to extract information for fields, methods,
+  properties and events for various objects pass to the single routine below.
+
+  @Author  David Hoyle
+  @Version 2.0
+  @Date    16 Apr 2016
+
+**)
+Unit DGHRTTIFunctions;
+
+Interface
+
+Uses
+  RTTI,
+  ComCtrls,
+  Classes;
+
+  Procedure ProcessObject(C : TObject; FieldView, MethodView, PropertyView,
+    EventView : TListView);
+  Procedure ProcessClass(tvTree : TTreeView; ParentNode : TTreeNode; C : TObject);
+
+Var
+  (** This class is used to store the addresses of all objects found so that we do not
+      iterate though loops in the IDE when getting classes. **)
+  FoundClasses : TList;
+
+Implementation
+
+Uses
+  SysUtils,
+  TypInfo,
+  Variants,
+  Graphics,
+  Controls;
+
+Const
+  (** This is a constant array of string to describe the different visibility aspects
+      of class members: private, protected, public and published. **)
+  strVisibility : Array[Low(TMemberVisibility)..High(TMemberVisibility)] Of String = (
+    'Private', 'Protected', 'Public', 'Published');
+
+(**
+
+  This function does all the conversion of a TValue passed to it into a text eqivalent.
+
+  @precon  None.
+  @postcon A striong representing the value is returned.
+
+  @param   Value as a TValue
+  @return  a String
+
+**)
+Function ValueToString(Value : TValue) : String;
+
+  (**
+
+    This function returns a square bracketed list of array values corresponding to the
+    array stored in Value.
+
+    @precon  None.
+    @postcon Returns a square bracketed list of array values.
+
+    @return  a String
+
+  **)
+  Function ArrayToString : String;
+
+  Var
+    iLength : Integer;
+    i : Integer;
+
+  Begin
+    Result := Value.ToString;
+    If Not Value.IsEmpty Then
+      Begin
+        Result := '';
+        iLength := Value.GetArrayLength;
+        For i := 0 To iLength - 1 Do
+          Begin
+            If Result <> '' Then
+              Result := Result + ', ';
+            Result := Result + ValueToString(Value.GetArrayElement(i));
+          End;
+        Result := '[' + Result + ']';
+      End;
+  End;
+
+  (**
+
+    This function returns the ordinal value of the character and if the character is
+    greater than 32 also appeands the character.
+
+    @precon  None.
+    @postcon Returns an ordinal value for the character.
+
+    @return  a String
+
+  **)
+  Function CharAsString : String;
+
+  Var
+    iOrd : Integer;
+
+  Begin
+    iOrd := Value.AsOrdinal;
+    Result := '#' + IntToStr(iOrd);
+    If iOrd >= 32 Then
+      Result := Result + ' (' + Chr(iOrd) + ')';
+  End;
+
+  (**
+
+    This function return the value of the method is assigned else returns (unassigned).
+
+    @precon  None.
+    @postcon Return the value of the method is assigned else returns (unassigned).
+
+    @return  a String
+
+  **)
+  Function MethodAsString : String;
+
+  Begin
+    If Value.IsEmpty Then
+      Result := '(unassigned)'
+    Else
+      Result := Value.ToString;
+  End;
+
+Begin
+  Try
+    Case Value.TypeInfo.Kind Of
+      tkUnknown:     Result := '<Unknown>';
+      tkInteger:     Result := Value.ToString;
+      tkChar:        Result := CharAsString;
+      tkEnumeration: Result := Value.ToString; // Boolean here
+      tkFloat:       Result := Format('#,##0.000', [Value.AsExtended]);
+      tkString:      Result := '''' + Value.AsString + '''';
+      tkSet:         Result := Value.ToString;
+      tkClass:       Result := Value.ToString;
+      tkMethod:      Result := MethodAsString;
+      tkWChar:       Result := CharAsString;
+      tkLString:     Result := '''' + Value.ToString + '''';
+      tkWString:     Result := '''' + Value.ToString + '''';
+      tkVariant:     Result := VarToStr(Value.AsVariant) + ' (' + VarTypeAsText(Value.AsVariant) + ')';
+      tkArray:       Result := ArrayToString;
+      tkRecord:      Result := Value.ToString;
+      tkInterface:   Result := Value.ToString;
+      tkInt64:       Result := IntToStr(Value.AsInt64);
+      tkDynArray:    Result := ArrayToString;
+      tkUString:     Result := '''' + Value.ToString + '''';
+      tkClassRef:    Result := Value.ToString;
+      tkPointer:     Result := Value.ToString;
+      tkProcedure:   Result := MethodAsString;
+    Else
+      Result := '<Not handled>';
+    End;
+  Except
+    On E : Exception Do
+      Result := 'Oops (' + E.ClassName + '): ' + E.Message;
+  End;
+End;
+
+(**
+
+  This procedure processes the retrieval of the value for fields and properties so that an
+  integer return can be overridden for TColor and TCursor to return their colours, cursor
+  names etc.
+
+  @precon  Item must be a valid instance.
+  @postcon The value of the field / property is added to the list view item.
+
+  @param   Item    as a TListItem
+  @param   Value   as a TValue
+  @param   strtype as a String
+
+**)
+Procedure ProcessValue(Item : TListItem; Value : TValue; strType : String);
+
+Var
+  strValue: String;
+
+Begin
+  strValue := ValueToString(Value);
+  If CompareText(strType, 'TColor') = 0 Then
+    strValue := ColorToString(Value.AsInteger);
+  If CompareText(strType, 'TCursor') = 0 Then
+    strValue := CursorToString(Value.AsInteger);
+  Item.SubItems.Add(strValue);
+End;
+
+(**
+
+  This procedure iterates through the properties of the given object outputting a list
+  view item for each property containing the following information:
+
+    Scope;
+    Fully Qualified Name;
+    Field Type;
+    Offset of the field in the VTable (I think);
+    Value TypeKind;
+    DataSize;
+    Value.
+
+  @precon  C, Ctx and View must be a valid instances.
+  @postcon A list view item is created for the property.
+
+  @param   C    as a TObject
+  @param   Ctx  as a TRTTIContext
+  @param   View as a TListView
+
+**)
+Procedure ProcessRTTIFields(C : TObject; Ctx : TRTTIContext; View : TListView);
+
+Var
+  T    : TRTTIType;
+  F    : TRTTIField;
+  V    : TValue;
+  Item : TListItem;
+
+Begin
+  View.Items.BeginUpdate;
+  Try
+    T := Ctx.GetType(C.ClassType);
+    For F In T.GetFields Do
+      Begin
+        Item := View.Items.Add;
+        Item.Caption := strVisibility[F.Visibility];
+        Item.StateIndex := Integer(F.Visibility);
+        Item.SubItems.Add(F.Parent.Name + '.' + F.Name);
+        Try
+          V := F.GetValue(C);
+          Item.SubItems.Add(F.FieldType.ToString);
+          Item.SubItems.Add(Format('$%x', [F.Offset]));
+          Item.SubItems.Add(GetEnumName(TypeInfo(TTypeKind), Ord(V.TypeInfo.Kind)));
+          Item.ImageIndex := Integer(V.TypeInfo.Kind);
+          Item.SubItems.Add(Format('%d ', [V.DataSize]));
+          ProcessValue(Item, V, F.FieldType.ToString);
+        Except
+          On E : Exception Do
+            Begin
+              while Item.SubItems.Count < 5 do
+                Item.SubItems.Add('');
+              Item.SubItems.Add('Oops (' + E.ClassName + '): ' + E.Message);
+            End;
+        End;
+      End;
+  Finally
+    View.Items.EndUpdate;
+  End;
+End;
+
+(**
+
+  This procedure iterates through the methods associated with the given object and output
+  a list view item for each method showing the following information:
+
+    Scope;
+    Fully Qualified Name;
+    Method Kind;
+    Method Signature.
+
+  @precon  C, Ctx and View must be valid instances.
+  @postcon A list view item is creates for each method.
+
+  @param   C    as a TObject
+  @param   Ctx  as a TRTTIContext
+  @param   View as a TListView
+
+**)
+Procedure ProcessRTTIMethods(C : TObject; Ctx : TRTTIContext; View : TListView);
+
+Var
+  T : TRTTIType;
+  M    : TRTTIMethod;
+  Item : TListItem;
+
+Begin
+  View.Items.BeginUpdate;
+  Try
+    T := Ctx.GetType(C.ClassType);
+    For M In T.GetMethods Do
+      Begin
+        Item := View.Items.Add;
+        Item.Caption := strVisibility[M.Visibility];
+        Item.StateIndex := Integer(M.Visibility);
+        Item.SubItems.Add(M.Parent.Name + '.' + M.Name);
+        Item.SubItems.Add(GetEnumName(TypeInfo(TMethodKind), Ord(M.MethodKind)));
+        Item.ImageIndex := Integer(tkMethod);
+        Item.SubItems.Add(M.ToString);
+      End;
+  Finally
+    View.Items.EndUpdate;
+  End;
+End;
+
+(**
+
+  This procedure is called for properties and event as the output is the same they are
+  just filtered differently. This outputs a list view item for the given property/event
+  in the object with the following information:
+
+    Scope;
+    Fully Qualified Name;
+    Property Type;
+    Access;
+    Value TypeKind;
+    DataSize;
+    Value.
+
+  @precon  C, P and View must be a valid instances.
+  @postcon A list view item is create the property or event.
+
+  @param   C    as a TObject
+  @param   P    as a TRTTIProperty
+  @param   View as a TListView
+
+**)
+Procedure ProcessRTTICoreProperty(C : TObject; P : TRTTIProperty; View : TListView);
+
+Var
+  V    : TValue;
+  Item : TListItem;
+
+Begin
+  Item := View.Items.Add;
+  Item.Caption := strVisibility[P.Visibility];
+  Item.StateIndex := Integer(P.Visibility);
+  Item.SubItems.Add(P.Parent.Name + '.' + P.Name);
+  Item.SubItems.Add(P.PropertyType.ToString);
+  If P.IsReadAble And P.IsWritable Then
+    Item.SubItems.Add('Read/Write')
+  Else If P.IsReadAble And Not P.IsWritable Then
+    Item.SubItems.Add('Readonly')
+  Else If P.IsReadAble And Not P.IsWritable Then
+    Item.SubItems.Add('Writeonly');
+  Try
+    V := P.GetValue(C);
+    Item.SubItems.Add(GetEnumName(TypeInfo(TTypeKind), Ord(V.TypeInfo.Kind)));
+    Item.ImageIndex := Integer(V.TypeInfo.Kind);
+    Item.SubItems.Add(Format('%d ', [V.DataSize]));
+    If P.IsReadable Then
+      ProcessValue(Item, V, P.PropertyType.ToString);
+  Except
+    On E : Exception Do
+      Begin
+        While Item.SubItems.Count < 5 Do
+          Item.SubItems.Add('');
+        Item.SubItems.Add('Oops (' + E.ClassName + '): ' + E.Message);
+      End;
+  End;
+End;
+
+(**
+
+  This procedure iterates through the properties of the object and creates a list view
+  item for each property not starting with "On" (i.e. an event).
+
+  @precon  C, Ctx and View must be valid instances.
+  @postcon A list view item is created for each property.
+
+  @param   C    as a TObject
+  @param   Ctx  as a TRTTIContext
+  @param   View as a TListView
+
+**)
+Procedure ProcessRTTIProperties(C : TObject; Ctx : TRTTIContext; View : TListView);
+
+Var
+  T    : TRTTIType;
+  P    : TRTTIProperty;
+
+Begin
+  View.Items.BeginUpdate;
+  Try
+    T := Ctx.GetType(C.ClassType);
+    For P In T.GetProperties Do
+      If CompareText(Copy(P.Name, 1, 2), 'on') <> 0 Then
+        ProcessRTTICoreProperty(C, P, View);
+  Finally
+    View.Items.EndUpdate;
+  End;
+End;
+
+(**
+
+  This procedure iterates through the properties of the object and creates a list view
+  item for each property starting with "On" (i.e. an event).
+
+  @precon  C, Ctx and View must be valid instances.
+  @postcon A list view item is created for each event.
+
+  @param   C    as a TObject
+  @param   Ctx  as a TRTTIContext
+  @param   View as a TListView
+
+**)
+Procedure ProcessRTTIEvents(C : TObject; Ctx : TRTTIContext; View : TListView);
+
+Var
+  T : TRTTIType;
+  P : TRTTIProperty;
+
+Begin
+  View.Items.BeginUpdate;
+  Try
+    T := Ctx.GetType(C.ClassType);
+    For P In T.GetProperties Do
+      If CompareText(Copy(P.Name, 1, 2), 'on') = 0 Then
+        ProcessRTTICoreProperty(C, P, View);
+  Finally
+    View.Items.EndUpdate;
+  End;
+End;
+
+(**
+
+  This procedure processes the fields, methods, properties adn events for the given
+  object.
+
+  @precon  C, FieldView, MethodView, PropertyView and EventView must be valid instances.
+  @postcon The fields, methods, properties and events of the object are output.
+
+  @param   C            as a TObject
+  @param   FieldView    as a TListView
+  @param   MethodView   as a TListView
+  @param   PropertyView as a TListView
+  @param   EventView    as a TListView
+
+**)
+Procedure ProcessObject(C : TObject; FieldView, MethodView, PropertyView,
+  EventView : TListView);
+
+Var
+  Ctx  : TRTTIContext;
+
+Begin
+  Ctx := TRttiContext.Create;
+  Try
+    ProcessRTTIFields(C, Ctx, FieldView);
+    ProcessRTTIMethods(C, Ctx, MethodView);
+    ProcessRTTIProperties(C, Ctx, PropertyView);
+    ProcessRTTIEvents(C, Ctx, EventView);
+  Finally
+    Ctx.Free;
+  End;
+End;
+
+(**
+
+  This procedure is design to recursively iterate through the classes and sub classes of
+  a given object and add them to the tree view.
+
+  @BUG     Using this code on the IDE (RAD Studio 10 Seattle) causes catastrophic
+           failures: It is assuemd that when TValue extracts the value of an item it
+           inadvertantly changes some of those obejcts which cause various AV and a
+           divide by zero error. DO NOT IMPLEMENT THIS UNTIL THE REASONS FOR THE FALIURES
+           ARE UNDERSTOOD OTHERWISE YOU WILL CRASH YOUR IDE.
+
+  @precon  tvTree, ParentNode and C must be valid instances.
+  @postcon Iterates through the classes subclasses adding them to the tree and then asking
+           those classes for their sub-classes.
+
+  @param   tvTree     as a TTreeView
+  @param   ParentNode as a TTreeNode
+  @param   C          as a TObject
+
+**)
+Procedure ProcessClass(tvTree : TTreeView; ParentNode : TTreeNode; C : TObject);
+
+Var
+  Ctx : TRttiContext;
+  T   : TRttiType;
+  F   : TRttiField;
+  P   : TRttiProperty;
+  V   : TValue;
+  N   : TTreeNode;
+  iIndex: Integer;
+
+Begin
+  FoundClasses.Add(C);
+  Ctx := TRttiContext.Create;
+  Try
+    Try
+      T := Ctx.GetType(C.ClassType);
+      For F In T.GetFields Do
+        If F.FieldType.TypeKind = tkClass Then
+          Begin
+            V := F.GetValue(C);
+            N := tvTree.Items.AddChildObject(ParentNode, F.Parent.Name + '.' + F.Name + ' : ' +
+              F.FieldType.ToString + ' ' + ValueToString(V), V.AsObject);
+            If Not V.IsEmpty Then
+              Begin
+                iIndex := FoundClasses.IndexOf(V.AsObject);
+                If iIndex = -1 Then
+                  ProcessClass(tvTree, N, V.AsObject);
+              End;
+          End;
+      For P In T.GetProperties Do
+        If P.PropertyType.TypeKind = tkClass Then
+          Begin
+            V := P.GetValue(C);
+            N := tvTree.Items.AddChildObject(ParentNode, P.Parent.Name + '.' + P.Name + ' : ' +
+              P.PropertyType.ToString + ' ' + ValueToString(V), V.AsObject);
+            If Not V.IsEmpty Then
+              Begin
+                iIndex := FoundClasses.IndexOf(V.AsObject);
+                If iIndex = -1 Then
+                  ProcessClass(tvTree, N, V.AsObject);
+              End;
+          End;
+    Except
+      On E : Exception Do
+        tvTree.Items.AddChild(ParentNode, '(' + E.ClassName + ')' + E.Message);
+    End;
+  Finally
+    Ctx.Free;
+  End;
+End;
+
+(** This initialization section is used to create a global list of objects to store
+    instances of found classes fir the ProcessClass method above. Its to prevent loops. **)
+Initialization
+  FoundClasses := TList.Create;
+(** Frees the FoudnClasses list. **)
+Finalization
+  FoundClasses.Free;
+End.
